d3b634d0fa170c5f656c285666c9e97c23f0c8c1
Eliott Francoise <>
2025-03-13 16:55:52 +0100

    Modified the data_preparation and data_cleaning

---
+++ f1_project/f1_packages/main.py
@@ -0,0 +1,13 @@
+from data_preparation import baseline_data_prep
+from data_cleaning import baseline_cleaning
+
+
+# Data preparation
+
+df = baseline_data_prep()
+
+# Data cleaning
+
+df_baseline = baseline_cleaning(df)
+
+df_baseline.to_csv("raw_data/df_baseline2.csv")
