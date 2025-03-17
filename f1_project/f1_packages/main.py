79ba0d91e2a75e06abf9ecc03ae8f0b635421bdd
Eliott Francoise <>
2025-03-17 15:36:04 +0100

    Changes to preparation and cleaning folders

--- f1_project/f1_packages/main.py
+++ f1_project/f1_packages/main.py
@@ -1,28 +1,30 @@
 from f1_project.f1_packages.data_preparation import *
 from f1_project.f1_packages.data_cleaning import *
 from f1_project.f1_packages.params import *
-from sklearn.model_selection import *
+from f1_project.f1_packages.preprocessor import *
 from f1_project.f1_packages.utils import *
 import pandas as pd
 
 
 def generation_dataframe(MODEL_VERSION="light"):
     if MODEL_VERSION == "light":
-        # Data preparation
-        df = baseline_data_prep()
         # Data cleaning
-        df_clean = light_cleaning(df)
+        df_clean = light_cleaning()
+        # Data preparation
+        df_good = baseline_data_prep(df_clean)
+
         # df_baseline.to_csv("raw_data/df_baseline2.csv", index=False)
 
     if MODEL_VERSION == "normal":
-        # Data preparation
-        df = normal_data_prep()
         # Data cleaning
-        df_clean = normal_cleaning(df)
+        df_clean = normal_cleaning()
+        # Data preparation
+        df_good = normal_data_prep(df_clean)
+
         # df_baseline.to_csv("raw_data/df_baseline2.csv", index=False)
 
     # save_to_csv(df_clean, "df_clean2")
-    return df_clean
+    return df_good
 
 
 def test_train_split(df):
@@ -33,15 +35,22 @@ def test_train_split(df):
 
     # Split data into train, test and validation sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
-    
+
     # Saving the datasets
     save_to_csv(pd.DataFrame(X_train), "X_train")
     save_to_csv(pd.DataFrame(X_test), "X_test")
-    
+
     # Returing X and y
     return X_train, X_test, y_train, y_test
 
 
-if __name__ == '__main__':
-    generation_dataframe(MODEL_VERSION)
-    test_train_split(generation_dataframe(MODEL_VERSION))
+# def preprocessing(X_train, X_test):
+#     Xx
+
+#     return X_train_preprocessed, X_test_preprocessed
+
+
+
+
+# if __name__ == '__main__':
+    # preprocessing(generation_dataframe(MODEL_VERSION))
