9d432b431fbdecbae958676a826ed4226d4663d8
fares abdeli <fares.abdeli@gmail.com>
2025-03-17 11:59:06 +0100

    ajout methode pour le features eng

--- f1_project/f1_packages/utils.py
+++ f1_project/f1_packages/utils.py
@@ -2,7 +2,8 @@ import pandas as pd
 import os
 
 def save_to_csv(df: pd.DataFrame, file_name):
-    df.to_csv(f"raw_data/{file_name}.csv")
+    url = '../../raw_data/'
+    df.to_csv(f"{url}{file_name}.csv")
 
 
 #def load_kaggle(): ## TO BE FINISHED
