56f1b382cee4209b513edbe1502cae250f23ea94
arthur autret <arthur.autret14@gmail.com>
2025-03-18 12:14:49 +0100

    update main

--- f1_project/f1_packages/main.py
+++ f1_project/f1_packages/main.py
@@ -57,7 +57,9 @@ def test_train_split(df):
         tuple: (X_train, X_test, y_train, y_test) - Training and testing splits
     """
     print("\n📊 Splitting data into train and test sets...")
-
+    for i in range(1 , NUMBER_OF_STOPS + 1) :
+        #df[f"pit{i}"] = df[f'pit{i}'][df[f'pit{i}'].isna()] = 0
+        df[f'pit{i}'].fillna(0, inplace=True)
     # Separate features (X) and target (y)
     print("1️⃣ Separating features and target variables...")
     X = df.drop(columns=["undercut_tentative", "undercut_success"])
